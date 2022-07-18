8afa86d4e4a999dcb00bfca9c0ca3b3783c3ae1f
Evangelos Paterakis <evan@geopjr.dev>
2022-07-18 04:13:57 +0300

    feat: bouncing arrow to indicate scroll

--- js/icon.js
+++ js/icon.js
@@ -3,9 +3,11 @@ import IconGithubSponsors from '~icons/simple-icons/githubsponsors'
 import IconCrystal from '~icons/simple-icons/crystal'
 import IconMastodon from '~icons/simple-icons/mastodon'
 import IconDownload from '~icons/charm/download'
+import IconDownArrow from '~icons/charm/chevron-down'
 
+document.getElementById('arrow-down').innerHTML = IconDownArrow
+document.getElementById('download').innerHTML = IconDownload
+document.getElementById('github').innerHTML = IconGithub
 document.getElementById('crystal').innerHTML = IconCrystal
 document.getElementById('mastodon').innerHTML = IconMastodon
 document.getElementById('ghsponsors').innerHTML = IconGithubSponsors
-document.getElementById('github').innerHTML = IconGithub
-document.getElementById('download').innerHTML = IconDownload
